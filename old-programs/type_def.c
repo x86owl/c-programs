734e032f308a05a208437e9bed748142e25170a9
itsmanikandan73 <soulquiet8@gmail.com>
2026-08-21 18:46:28 +0530

    adding type_def.c program

--- old-programs/type_def.c
+++ old-programs/type_def.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef int numbers;
-typedef char string[30];
-typedef char initials[3];
+typedef char strings[15];
+typedef char shells[8];
 
-int main(){
-    // typedef existing_type new_type;
-    /*numbers x = 3;
-    numbers y = 5;
+int main(int argc, char *argv[]) {     
+    numbers x = 5;
+    numbers y = 8;
     numbers z = x + y;
-    printf("%d\n", z);
+    fprintf(stdout, "%d\n", z);
 
-    
-    string name = "manikandan";
-    printf("%s\n", name);*/
+    strings name = "x86owl";
+    fprintf(stdout,"%s\n", name);
 
-    initials user1 = "JK";
-    initials user2 = "MR";
-    initials user3 = "RR";
-    initials user4 = "SK";
+    shells user1 = "bash";
+    fprintf(stdout,"%s\n", user1);
 
-    printf("%s\n", user1);
-    printf("%s\n", user2);
-    printf("%s\n", user3);
-    printf("%s\n", user4);
-
-
-    return 0;
+    return EXIT_SUCCESS;
 }
